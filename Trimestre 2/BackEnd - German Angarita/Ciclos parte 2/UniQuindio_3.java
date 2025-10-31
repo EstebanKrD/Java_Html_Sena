0a7903cbf07467f841aae8500b6a72fd83dd16d6
EstebanKrD <krdonaj@gmail.com>
2025-10-31 12:42:35 -0500

    feat: se agrega nuevos ejercicos

---
+++ Trimestre 2/BackEnd - German Angarita/Ciclos parte 2/UniQuindio_3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UniQuindio_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        int contador = 0;
+
+
+    
+    }
+}
