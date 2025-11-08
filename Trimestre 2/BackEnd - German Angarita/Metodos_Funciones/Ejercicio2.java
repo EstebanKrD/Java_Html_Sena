b4b997d091e4a916d91da9e87383c27c9b209cb2
EstebanKrD <krdonaj@gmail.com>
2025-11-08 10:48:08 -0500

    feat: se realiza ejercicio de validacion de correo y contraseña

---
+++ Trimestre 2/BackEnd - German Angarita/Metodos_Funciones/Ejercicio2.java
@@ -0,0 +1,27 @@
+// Crear un programa Java que valide el registro de un usuario. El usuario debe introducir un email y una contraseña. El sistema debe verificar que el email tenga formato de email(que tenga @ y un .), y la contraseña debe tener entre 6 y 10 caracteres. El sistema debe pedir los datos al usuario indefinidamente hasta que el registro se haga bien. Para la validación, use una función o método llamada Validator. Cuando el usuario se registre exitosamente, debe mostrase un mensaje que diga "Usuario ADSO, su registro ha sido exitoso", para mostrar este mensaje debe usar otra función(método)
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String correo;
+        String password;
+
+        do {
+            System.out.println("Ingrese su correo: ");
+            correo = scanner.nextLine();
+        } while (!Validator.validarCorreo(correo));
+
+        do {
+            System.out.println("Ingrese su contraseña: ");
+            password = scanner.nextLine();
+        } while (!Validator.validarPassword(password));
+
+        // 3. Si todo fue correcto
+        Validator.registroExitoso();
+
+        scanner.close();
+    }
+}
